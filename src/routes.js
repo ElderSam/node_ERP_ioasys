49de8114bb5ef203571b7964b9d5efa1847846cc
ElderSam <eldersamuel98@gmail.com>
2022-03-28 14:46:37 -0300

    feat: create authMiddleware & add to authenticated routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 const routes = require("express").Router();
 
 const SessionController = require('./app/controllers/SessionController')
-const authMiddleware = require('./app/middleware/auth')
+const authMiddleware = require('./app/middleware/auth');
 
 // Routes definition
 routes.post('/sessions', SessionController.store)
 
+/* ------------------ Authenticated routes ------------------ */
+routes.use(authMiddleware)
+
 routes.get('/dashboard', (req, res) => {
     res.status(200).send()
 })
-
-module.exports = routes;
