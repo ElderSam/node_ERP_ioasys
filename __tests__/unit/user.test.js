dab39dbcf595d129df02358dc4fc66011c5e73b0
ElderSam <eldersamuel98@gmail.com>
2022-03-28 13:27:55 -0300

    test: add password encrypt unit test

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcryptjs");
+
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+
+describe("", () => {
+	beforeEach(async () => {
+		await truncate();
+	});
+
+	it("should encrypt user password", async () => {
+		const user = await User.create({
+			name: "Samuel",
+			email: "sam@gmail.com",
+			password: "123456",
+			isAdmin: 0
+		});
+
+		const hash = await bcrypt.hash("123456", 8);
+
+        expect(user.password_hash).toBe(hash);
+	});
+});
