c7db4f884894399be28af9f81546b8ce678da96e
ElderSam <eldersamuel98@gmail.com>
2022-03-28 15:01:20 -0300

    fix: factory import

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,19 +1,18 @@
 const request = require("supertest");
 
 const app = require("../../src/app");
-const truncate = require("../utils/truncate")
+const truncate = require("../utils/truncate");
+const factory = require("../factories");
 
 describe("Authentication", () => {
 	beforeEach(async () => {
 		await truncate();
-	})
+	});
 
 	it("should authenticate with valid credentials", async () => {
 		const user = await factory.create("User", {
 			password: "123123",
 		});
-
-		expect(user.email).toBe("test@gmail.com.br")
 
 		const response = await request(app).post("/sessions").send({
 			email: user.email,
@@ -56,7 +55,7 @@ describe("Authentication", () => {
 			.get("/dashboard")
 			.set("Authorization", `Bearer ${user.generateToken()}`);
 
-		expect(response.status).toBe(200)
+		expect(response.status).toBe(200);
 	});
 
 	it("should not be able to access private routes without JWT token", async () => {
