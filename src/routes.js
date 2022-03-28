f4d46a93ef241fa5e4b61e32e592a9aca4d87a00
ElderSam <eldersamuel98@gmail.com>
2022-03-28 13:25:54 -0300

    feat: create route /sessions

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
-const routes = require('express').Router()
+const routes = require("express").Router();
+const SessionController = require('./app/controllers/SessionController')
 
 // Routes definition
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes;
